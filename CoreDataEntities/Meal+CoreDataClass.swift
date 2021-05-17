093351e423e36225e4a910a566cdd831841df2cc
Olesia <odobrogor@gmail.com>
2021-05-17 19:42:32 +0300

    Create CoreDataModel

---
+++ CoreDataEntities/Meal+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Олеся Егорова on 17.05.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
---
+++ CoreDataEntities/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Олеся Егорова on 17.05.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
